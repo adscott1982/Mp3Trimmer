910061fd9496042556cc40f0a78d101995705aee
ascott <ascott@visionrt.com>
2016-12-22 17:03:52 +0000

    - Cosmetic changes to view - Fixed references to WPF toolkit (uses Nuget now) - Fixed starting folder for folder browser dialog

--- Mp3Trimmer/Mp3TrimmerViewViewModel.cs
+++ Mp3Trimmer/Mp3TrimmerViewViewModel.cs
@@ -19,6 +19,8 @@ namespace Mp3Trimmer
         // TODO Implement callbacks from trimming task into log
         // TODO Change timer controls to numeric up down for hour/min/sec
 
+        private string currentPath;
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         public ICommand LoadMp3FileCommand { get; }
@@ -244,6 +246,8 @@ namespace Mp3Trimmer
 
             SplitCount = 1;
             IsIdle = true;
+
+            this.currentPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
         }
 
         #endregion
@@ -259,12 +263,13 @@ namespace Mp3Trimmer
         {
             var openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3";
-            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFileDialog.InitialDirectory = this.currentPath;
 
             if (openFileDialog.ShowDialog() == true)
             {
                 IsIdle = false;
 
+                this.currentPath = Path.GetDirectoryName(openFileDialog.FileName);
                 this.Mp3FileLoaded = await Task.Run<Mp3File>(() => new Mp3File(openFileDialog.FileName));
 
                 IsIdle = true;
@@ -279,7 +284,7 @@ namespace Mp3Trimmer
         private void SelectFolder(object obj)
         {
             var folderDialog = new VistaFolderBrowserDialog();
-            folderDialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            folderDialog.SelectedPath = this.currentPath + @"\";
             folderDialog.Description = "Select the output folder for the trimmed MP3 file(s)";
             folderDialog.ShowNewFolderButton = true;
             folderDialog.UseDescriptionForTitle = true;
