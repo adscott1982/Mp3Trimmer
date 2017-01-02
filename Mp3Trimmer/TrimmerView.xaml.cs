a063dcd278adca47bc12faa87d67fcd1a9d431aa
Andy Scott <adscott1982@googlemail.com>
2017-01-02 22:02:04 +0000

    Refactoring code to make trimmerview a user control, and reference it in a separate window, using ContentControl. Work in progress.

---
+++ Mp3Trimmer/TrimmerView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mp3Trimmer
+{
+    /// <summary>
+    /// Interaction logic for TrimmerView.xaml
+    /// </summary>
+    public partial class TrimmerView : UserControl
+    {
+        public Mp3TrimmerViewViewModel ViewModel { get; }
+
+        public TrimmerView()
+        {
+            InitializeComponent();
+            this.ViewModel = new Mp3TrimmerViewViewModel();
+            this.DataContext = this.ViewModel;
+        }
+    }
+}
